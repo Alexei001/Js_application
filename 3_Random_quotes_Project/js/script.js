cfac4804795798013cf496198b29af244f2ef971
Alexei001 <alexeiyoyo12@gmail.com>
2020-12-14 05:35:50 +0200

    new_project_random_quotes_machines

---
+++ 3_Random_quotes_Project/js/script.js
@@ -0,0 +1,16 @@
+
+
+let autorArr = [
+{text:"If life were predictable it would cease to be life, and be without flavor.", author:"-Eleanor Roosevelt"},
+{text:"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",author:"-Steve Jobs"},
+{text:"The way to get started is to quit talking and begin doing.", author:"-Walt Disney"},
+{text:"The greatest glory in living lies not in never falling, but in rising every time we fall.",author:"-Nelson Mandela"},
+/*"Tell me and I forget. Teach me and I remember. Involve me and I learn." -Benjamin Franklin
+"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart." -Helen Keller
+"Do not go where the path may lead, go instead where there is no path and leave a trail." -Ralph Waldo Emerson
+"It is during our darkest moments that we must focus to see the light." -Aristotle
+"The future belongs to those who believe in the beauty of their dreams." -Eleanor Roosevelt
+"Don't judge each day by the harvest you reap but by the seeds that you plant." -Robert Louis Stevenson
+"When you reach the end of your rope, tie a knot in it and hang on." -Franklin D. Roosevelt
+*/
+}
